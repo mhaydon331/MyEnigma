b9b03ee604fb6a4e9304f2483aa1b052133e2c30
mhaydon331 <70169970+mhaydon331@users.noreply.github.com>
2022-11-19 11:25:52 -0700

    Added functionality for new rotors
    Added rotors 6-8 and new tests for these

--- test_enigma.py
+++ test_enigma.py
@@ -16,47 +16,58 @@ def test_init():
         cipher = Enigma(rotors = [["II",0],["II",0],["I",0]], reflector = "UKW-A", plugboard = "AV BS CG DL FU HZ IN KM OW RX")
     #test incorrect rotor specified exits
     with pytest.raises(SystemExit) as excinfo:
-        cipher = Enigma(rotors = [["I",0],["II",0],["VI",0]], reflector = "UKW-A", plugboard = "AV BS CG DL FU HZ IN KM OW RX")
+        cipher = Enigma(rotors = [["I",0],["II",0],["VXI",0]], reflector = "UKW-A", plugboard = "AV BS CG DL FU HZ IN KM OW RX")
     #test incorrect reflector specified exits
     with pytest.raises(SystemExit) as excinfo:
         cipher = Enigma(rotors = [["I",0],["II",0],["III",0]], reflector = "UKW-W", plugboard = "AV BS CG DL FU HZ IN KM OW RX")
 
 def test_plugboard():
-    #test incorrect plugboard used sets default plygboard
+    #test all one string only uses first two settings according to pyenigma
     plug_board = Plugboard("NKMOWRX")
-    assert plug_board.plugboard == {'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G', 'H': 'H', 'I': 'I', 'J': 'J', 'K': 'K', 'L': 'L', 'M': 'M', 'N': 'N', 'O': 'O', 'P': 'P', 'Q': 'Q', 'R': 'R', 'S': 'S', 'T': 'T', 'U': 'U', 'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z'}
+    assert plug_board.plugboard == {'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G', 'H': 'H', 'I': 'I', 'J': 'J', 'K': 'N', 'L': 'L', 'M': 'M', 'N': 'K', 'O': 'O', 'P': 'P', 'Q': 'Q', 'R': 'R', 'S': 'S', 'T': 'T', 'U': 'U', 'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z'}
     #test incorrect plugboard (duplicate plugs attempted) used sets default plygboard
     plug_board = Plugboard("AN MP BQ AK")
     assert plug_board.plugboard == {'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G', 'H': 'H', 'I': 'I', 'J': 'J', 'K': 'K', 'L': 'L', 'M': 'M', 'N': 'N', 'O': 'O', 'P': 'P', 'Q': 'Q', 'R': 'R', 'S': 'S', 'T': 'T', 'U': 'U', 'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z'}
+
+def test_plugboard_v_pyengima():
     #testing plugboard against pyenigma
     engine = enigma.Enigma(rotor.ROTOR_Reflector_A, rotor.ROTOR_I,
                                 rotor.ROTOR_II, rotor.ROTOR_III, key="AAA",
                                 plugs="AV BS CG DL FU HZ IN KM OW RX")
 
-    cipher = Enigma(rotors = [["III",0],["II",0],["I",0]], reflector = "UKW-A",
-                                plugboard = "AV BS CG DL FU HZ IN KM OW RX")
+    plug_board = Plugboard("AV BS CG DL FU HZ IN KM OW RX")
     plugboard = {}
-    for k,v in cipher.plugboard_map.plugboard.items():
+    for k,v in plug_board.plugboard.items():
         plugboard[ord(k)] = ord(v)
     assert engine.transtab == plugboard
     engine = enigma.Enigma(rotor.ROTOR_Reflector_A, rotor.ROTOR_I,
                                 rotor.ROTOR_II, rotor.ROTOR_III, key="AAA",
                                 plugs="")
 
-    cipher = Enigma(rotors = [["III",0],["II",0],["I",0]], reflector = "UKW-A",
-                                plugboard = "")
+    plug_board = Plugboard("")
     plugboard = {}
-    for k,v in cipher.plugboard_map.plugboard.items():
+    for k,v in plug_board.plugboard.items():
+        plugboard[ord(k)] = ord(v)
+    assert engine.transtab == plugboard
+    engine = enigma.Enigma(rotor.ROTOR_Reflector_A, rotor.ROTOR_I,
+                                rotor.ROTOR_II, rotor.ROTOR_III, key="AAA",
+                                plugs="AV BS CG DL FU HZ IN KMOWRX")
+    plug_board = Plugboard("AV BS CG DL FU HZ IN KMOWRX")
+    plugboard = {}
+    for k,v in plug_board.plugboard.items():
         plugboard[ord(k)] = ord(v)
     assert engine.transtab == plugboard
 
 
-def test_rotor():
+def test_rotor_v_pyenigma():
     assert Rotor(["I",0]).rotor_order == rotor.ROTOR_I.wiring
     assert Rotor(["II",0]).rotor_order == rotor.ROTOR_II.wiring
     assert Rotor(["III",0]).rotor_order == rotor.ROTOR_III.wiring
     assert Rotor(["IV",0]).rotor_order == rotor.ROTOR_IV.wiring
     assert Rotor(["V",0]).rotor_order == rotor.ROTOR_V.wiring
+    assert Rotor(["VI",0]).rotor_order == rotor.ROTOR_VI.wiring
+    assert Rotor(["VII",0]).rotor_order == rotor.ROTOR_VII.wiring
+    assert Rotor(["VIII",0]).rotor_order == rotor.ROTOR_VIII.wiring
 
 def test_against_pyenigma():
     engine = enigma.Enigma(rotor.ROTOR_Reflector_A, rotor.ROTOR_I,
